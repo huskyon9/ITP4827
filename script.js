221cbca28d70d24d71638edd202d58bac2f0b3a3
huskyon9 <tinsang@hotmail.com.hk>
2024-02-22 18:57:33 +0800

    recode 2

--- script.js
+++
@@ -1,9 +0,0 @@
-
-<script async src="https://www.googletagmanager.com/gtag/js?id=G-ZC4N730V0J"></script>
-<script>
-  window.dataLayer = window.dataLayer || [];
-  function gtag(){dataLayer.push(arguments);}
-  gtag('js', new Date());
-
-  gtag('config', 'G-ZC4N730V0J');
-</script>
